627401c67352345a86ecb564ee4c0278be1012fa
Younghoda <141108118+Younghoda@users.noreply.github.com>
2023-12-06 12:36:56 +0900

    [level unrated] Title: 진료 순서 정하기, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120835. 진료 순서 정하기/진료 순서 정하기.py
@@ -0,0 +1,9 @@
+def solution(emergency):
+    answer = []
+    
+    temp = sorted(emergency, reverse=True)
+    
+    for i in emergency:
+        answer.append(temp.index(i)+1)
+    
+    return answer
